bf9d428c144883cb27abaf69383b4224a892b7e4
mskKote <msk.vitaly@gmail.com>
2024-07-29 05:23:36 +0200

    feat: pages structure

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,10 +1,67 @@
+import { Transition } from "@headlessui/react";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+const projects = [
+  { name: "Проект 1", path: `project1` },
+  { name: "Проект 2", path: `project2` },
+  { name: "Проект 3", path: `project3` },
+  { name: "Проект 4", path: `project4` },
+  { name: "Проект 5", path: `project5` }
+];
+
 function CategoryPage() {
+  const { category } = useParams<{ category: string }>();
+  console.log(category, `${category}${projects[1].path}`);
+
+  const [show, setShow] = useState<boolean[]>(
+    Array(projects.length).fill(false)
+  );
+
+  useEffect(() => {
+    console.log("useEffect");
+    projects.forEach((_, index) => {
+      setTimeout(
+        () => {
+          setShow((prev) => {
+            const newShow = [...prev];
+            newShow[index] = true;
+            return newShow;
+          });
+        },
+        (1 + index) * 350
+      ); // Adjust the delay as needed
+    });
+  }, []);
+
   return (
-    <div className="min-h-screen relative">
-      Страница категории
-      <main className="h-screen pt-24 lg:pt-0 relative z-0">
-        <div className="w-screen h-full">{/* TODO: button start */}</div>
-      </main>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex items-stretch justify-center flex-wrap content-center gap-4 p-4">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="relative"
+            style={{ height: "200px", width: "300px" }}
+          >
+            <Transition
+              show={show[index]}
+              enter="transition transform ease-out duration-500"
+              enterFrom="translate-y-10 opacity-0"
+              enterTo="translate-y-0 opacity-100"
+            >
+              <div className="absolute inset-0">
+                <div
+                  className="bg-gray-700 text-white p-6 rounded-lg flex items-center justify-center text-center hover:bg-gray-600 transition-all duration-500"
+                  style={{ height: "100%", width: "100%" }}
+                >
+                  <Link to={`/${category}/${project.path}`}>
+                    {project.name}
+                  </Link>
+                </div>
+              </div>
+            </Transition>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,61 @@
+import { Transition } from "@headlessui/react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+const categories = [
+  { name: "Новые материалы и металлы", path: "/new-materials" },
+  { name: "Системы и агрегаты техники", path: "/systems" },
+  { name: "Электроника и информационные технологии", path: "/electronics" },
+  { name: "Охрана и физическая защита", path: "/security" },
+  { name: "Малая энергетика", path: "/energy" },
+  { name: "Здравоохранение", path: "/healthcare" }
+];
+
 function HomePage() {
+  const [show, setShow] = useState<boolean[]>(
+    Array(categories.length).fill(false)
+  );
+  useEffect(() => {
+    categories.forEach((_, index) => {
+      setTimeout(
+        () => {
+          setShow((prev) => {
+            const newShow = [...prev];
+            newShow[index] = true;
+            return newShow;
+          });
+        },
+        (1 + index) * 350
+      ); // Adjust the delay as needed
+    });
+  }, []);
+
   return (
-    <div className="min-h-screen relative">
-      <main className="h-screen pt-24 lg:pt-0 relative z-0">
-        <div className="w-screen h-full">{/* TODO: button start */}</div>
-      </main>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+        {categories.map((category, index) => (
+          <div
+            key={index}
+            className="relative"
+            style={{ height: "200px", width: "300px" }}
+          >
+            <Transition
+              show={show[index]}
+              enter="transition transform ease-out duration-500"
+              enterFrom="translate-y-10 opacity-0"
+              enterTo="translate-y-0 opacity-100"
+            >
+              <div className="absolute inset-0">
+                <div
+                  className="bg-gray-700 text-white p-6 rounded-lg flex items-center justify-center text-center hover:bg-gray-600 transition-all duration-500"
+                  style={{ height: "100%", width: "100%" }}
+                >
+                  <Link to={category.path}>{category.name}</Link>
+                </div>
+              </div>
+            </Transition>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
